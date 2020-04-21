c75fd841dd0b6f122f932e841027825dd9cf93a6
Prince <prince17.ps6@gmail.com>
2020-04-22 02:01:11 +0530

    todo setup

---
+++ lib/presentations/todo_list/todo_card.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/models/todo.dart';
+
+class TodoCard extends StatelessWidget {
+  final Todo todo;
+  TodoCard(this.todo);
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
